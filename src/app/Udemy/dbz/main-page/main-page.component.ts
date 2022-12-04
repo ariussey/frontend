6ac5c7cae25bd2a29c3155baac441e3a01eec7fa
Angemar <angemar2424@gmail.com>
2022-12-04 09:08:01 -0500

    curso udemy

--- src/app/Udemy/dbz/main-page/main-page.component.ts
+++ src/app/Udemy/dbz/main-page/main-page.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
 
+
+  personajes : Personaje [] = [
+    {
+      nombre: 'Goku',
+      poder: 150000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
   agregar(){
-    console.log('Hey!!')
+    if (this.nuevo.nombre.trim().length === 0) { return; }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {nombre: '', poder: 0}
+    console.log(this.nuevo)
   }
 
 }
